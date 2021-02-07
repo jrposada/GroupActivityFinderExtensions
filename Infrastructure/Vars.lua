00e51d8ae7dda82b196758b6eb235e5ba52afdb7
Javier Rodriguez Posada <javiropos@gmail.com>
2021-02-07 23:04:02 +0100

    Increase only the minor version

--- Infrastructure/Vars.lua
+++ Infrastructure/Vars.lua
@@ -1,6 +1,6 @@
 GroupActivityFinderExtensions = {
     name = "GroupActivityFinderExtensions",
-    version = 3,
+    version = 2.1,
     varsVersion = 2,
     Localization = {},
     Loc	= function(var) return GroupActivityFinderExtensions.Localization[var] or var end,
