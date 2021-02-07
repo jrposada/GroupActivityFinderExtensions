af197747849299898123fa898df7243105f25787
Javier Rodriguez Posada <javiropos@gmail.com>
2021-02-07 19:03:05 +0100

    French localization

---
+++ lang/fr.lua
@@ -0,0 +1,9 @@
+local GAFE = GroupActivityFinderExtensions
+
+GAFE.Localization = {
+    Settings_Description = "Ajoute un peu de fonctionnalité et d'information a UI de Groupe et Recherche d'activités",
+    Settings_AutoConfirm = "Bouton 'Auto confirmer'",
+    CheckActivePledges = "Sélect serment activ",
+    CheckMissingQuests = "Sélect quêtes incompl",
+    AutoConfirm = "Auto confirmer"
+}
