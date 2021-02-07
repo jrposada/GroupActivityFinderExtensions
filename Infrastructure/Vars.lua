d9f5e0dfdfc874a9aaa75116b33e3adb8c85f6c0
Javier Rodriguez Posada <javiropos@gmail.com>
2021-02-07 22:59:49 +0100

    Increase version

--- Infrastructure/Vars.lua
+++ Infrastructure/Vars.lua
@@ -1,6 +1,6 @@
 GroupActivityFinderExtensions = {
     name = "GroupActivityFinderExtensions",
-    version = 2,
+    version = 3,
     varsVersion = 2,
     Localization = {},
     Loc	= function(var) return GroupActivityFinderExtensions.Localization[var] or var end,
