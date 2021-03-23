1f57ff50d4354adca4702426cac130789387ac56
Javier Rodriguez Posada <javiropos@gmail.com>
2021-03-23 12:24:25 +0100

    Increase version

--- Infrastructure/Vars.lua
+++ Infrastructure/Vars.lua
@@ -3,7 +3,7 @@ GAFE_LFG_ACTIVITY_MASTER_TRIAL = LFG_ACTIVITY_ITERATION_END + 2
 
 GroupActivityFinderExtensions = {
     name = "GroupActivityFinderExtensions",
-    version = 4.2,
+    version = 4.3,
     varsVersion = 3,
     Localization = {},
     Loc	= function(var) return GroupActivityFinderExtensions.Localization[var] or var end,
