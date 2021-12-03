2e336e4c89cb5436d060c574da6681c6177ea578
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2021-12-03 21:34:50 +0100

    Increase version to 4.13 before release

--- Infrastructure/Vars.lua
+++ Infrastructure/Vars.lua
@@ -1,6 +1,6 @@
 GroupActivityFinderExtensions = {
     name = "GroupActivityFinderExtensions",
-    version = 4.12,
+    version = 4.13,
     varsVersion = 3,
     Localization = {},
     Loc	= function(var) return GroupActivityFinderExtensions.Localization[var] or var end,
