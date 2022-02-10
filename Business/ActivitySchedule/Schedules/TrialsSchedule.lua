7eabd54bd5fc8b74ee13de1c1ca716a369ed361f
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2022-02-10 14:19:30 +0100

    Disable schedule

--- Business/ActivitySchedule/Schedules/TrialsSchedule.lua
+++ Business/ActivitySchedule/Schedules/TrialsSchedule.lua
@@ -14,8 +14,7 @@ end
 function GAFE_TrialsSchedule:Initialize(control)
     self.control = control
 
-    self.scroll = self.control:GetNamedChild("Scroll")
-    self.scrollChild = self.scroll:GetNamedChild("ScrollChild")
+    self.container = self.control:GetNamedChild("Container")
 
     self:InitializeControls()
 end
@@ -31,10 +30,12 @@ function GAFE_TrialsSchedule:InitializeFragment()
         rowControl:SetFont("ZoFontWinH4")
     end
 
+    local scrollChild = self.container:GetNamedChild("ScrollChild")
+
     -- Create the scroll list
     local scrollData = {
         name    = "MyTestScrollList",
-        parent  = self.scrollChild,
+        parent  = scrollChild,
         -- width   = 300,
         -- height  = 500,
 
@@ -52,32 +53,37 @@ function GAFE_TrialsSchedule:InitializeFragment()
         -- categories  = {1, 2},
     }
 
-    self.scrollList = libScroll:CreateScrollList(scrollData)
-    self.scrollList:SetAnchor(TOPLEFT, self.scrollChild, TOPLEFT, 0, 0)
-    self.scrollList:SetAnchor(BOTTOMRIGHT, self.scrollChild, BOTTOMRIGHT, 50, 0)
+    -- self.scrollList = libScroll:CreateScrollList(scrollData)
+    -- self.scrollList:SetAnchor(TOPLEFT, scrollChild, TOPLEFT, 0, 0)
+    -- self.scrollList:SetAnchor(BOTTOMRIGHT, scrollChild, BOTTOMRIGHT, 50, 0)
+
 
     -- Add data to scroll list.
-    local dataItems = {
-    }
-    for i=1,100 do
-        dataItems[i] = {name= i..'asdf asdf asdf asdfasdfasdfa sdfa sdfa sdfa  '}
-    end
-    self.scrollList:Update(dataItems)
+    -- local dataItems = {
+    -- }
+    -- for i=1,100 do
+    --     local data = {name= i..'asdf asdf asdf asdfasdfasdfa sdfa sdfa sdfa  '}
+
+        
+    -- end
+    GAFE.UI.ZOButton("GAFE_PledgesCheck", scrollChild, {200, 200}, {TOP,scrollChild,TOP,0,0}, GAFE.Loc("CheckActivePledges"))
+
+    -- self.scrollList:Update(dataItems)
 
     --     h5. ScrollBounding
     -- * SCROLL_BOUNDING_BOUND
     -- * SCROLL_BOUNDING_CONTAINED
     -- * SCROLL_BOUNDING_DEFAULT
     -- * SCROLL_BOUNDING_UNBOUND
-    self.scroll:SetScrollBounding(SCROLL_BOUNDING_UNBOUND)
-    local scrollWidth = self.scroll:GetWidth()
-    local currentHorizonalScrollOffset, currentVerticalScrollOffset = self.scroll:GetScrollOffsets()
-    GAFE.LogLater("width | "..scrollWidth)
-    -- Scroll the selected lore entry to the center of the scroll control's view.
-    local controlOffset = self.scrollList:GetTop()
-    local controlWidth = self.scrollList:GetWidth()
-    local targetOffset = currentHorizonalScrollOffset + controlOffset - 0.5 * (scrollWidth - controlWidth)
-    self.scroll:SetHorizontalScroll(20)
+    -- self.scroll:SetScrollBounding(SCROLL_BOUNDING_UNBOUND)
+    -- local scrollWidth = self.scroll:GetWidth()
+    -- local currentHorizonalScrollOffset, currentVerticalScrollOffset = self.scroll:GetScrollOffsets()
+    -- GAFE.LogLater("width | "..scrollWidth)
+    -- -- Scroll the selected lore entry to the center of the scroll control's view.
+    -- local controlOffset = self.scrollList:GetTop()
+    -- local controlWidth = self.scrollList:GetWidth()
+    -- local targetOffset = currentHorizonalScrollOffset + controlOffset - 0.5 * (scrollWidth - controlWidth)
+    -- self.scroll:SetHorizontalScroll(20)
 end
 
 function GAFE_TrialsSchedule_Init(control)
