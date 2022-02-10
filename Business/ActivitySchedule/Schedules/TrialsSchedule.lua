fb3fb9ce552d0bea0123bb815378e959c81905f7
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2022-02-10 12:12:17 +0100

    In progress

--- Business/ActivitySchedule/Schedules/TrialsSchedule.lua
+++ Business/ActivitySchedule/Schedules/TrialsSchedule.lua
@@ -1,5 +1,6 @@
 local GAFE = GroupActivityFinderExtensions
 local WM = WINDOW_MANAGER
+local libScroll = LibScroll
 -- local TrialQuest = GAFE.TrialChestTimer.TrialQuest
 
 GAFE_TrialsSchedule = ZO_Object:Subclass()
@@ -13,7 +14,8 @@ end
 function GAFE_TrialsSchedule:Initialize(control)
     self.control = control
 
-    self.label = self.control:GetNamedChild("Label")
+    self.scroll = self.control:GetNamedChild("Scroll")
+    self.scrollChild = self.scroll:GetNamedChild("ScrollChild")
 
     self:InitializeControls()
 end
@@ -23,7 +25,59 @@ function GAFE_TrialsSchedule:InitializeControls()
 end
 
 function GAFE_TrialsSchedule:InitializeFragment()
-    self.label:SetText('Initialized')
+    local function SetupDataRow(rowControl, data, scrollList)
+        -- Do whatever you want/need to setup the control
+        rowControl:SetText(data.name)
+        rowControl:SetFont("ZoFontWinH4")
+    end
+
+    -- Create the scroll list
+    local scrollData = {
+        name    = "MyTestScrollList",
+        parent  = self.scrollChild,
+        -- width   = 300,
+        -- height  = 500,
+
+        -- rowHeight       = 23,
+        -- rowTemplate     = "EmoteItRowControlTemplate",
+        setupCallback   = SetupDataRow,
+        -- sortFunction    = SortScrollList,
+        -- selectTemplate  = "EmoteItSelectTemplate",
+        -- selectCallback  = OnRowSelection,
+
+        -- dataTypeSelectSound = SOUNDS.BOOK_CLOSE,
+        -- hideCallback    = OnRowHide,
+        -- resetCallback   = OnRowReset,
+
+        -- categories  = {1, 2},
+    }
+
+    self.scrollList = libScroll:CreateScrollList(scrollData)
+    self.scrollList:SetAnchor(TOPLEFT, self.scrollChild, TOPLEFT, 0, 0)
+    self.scrollList:SetAnchor(BOTTOMRIGHT, self.scrollChild, BOTTOMRIGHT, 50, 0)
+
+    -- Add data to scroll list.
+    local dataItems = {
+    }
+    for i=1,100 do
+        dataItems[i] = {name= i..'asdf asdf asdf asdfasdfasdfa sdfa sdfa sdfa  '}
+    end
+    self.scrollList:Update(dataItems)
+
+    --     h5. ScrollBounding
+    -- * SCROLL_BOUNDING_BOUND
+    -- * SCROLL_BOUNDING_CONTAINED
+    -- * SCROLL_BOUNDING_DEFAULT
+    -- * SCROLL_BOUNDING_UNBOUND
+    self.scroll:SetScrollBounding(SCROLL_BOUNDING_UNBOUND)
+    local scrollWidth = self.scroll:GetWidth()
+    local currentHorizonalScrollOffset, currentVerticalScrollOffset = self.scroll:GetScrollOffsets()
+    GAFE.LogLater("width | "..scrollWidth)
+    -- Scroll the selected lore entry to the center of the scroll control's view.
+    local controlOffset = self.scrollList:GetTop()
+    local controlWidth = self.scrollList:GetWidth()
+    local targetOffset = currentHorizonalScrollOffset + controlOffset - 0.5 * (scrollWidth - controlWidth)
+    self.scroll:SetHorizontalScroll(20)
 end
 
 function GAFE_TrialsSchedule_Init(control)
