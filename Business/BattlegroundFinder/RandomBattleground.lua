37f3b96285b62bb2c9ce14d85b991bc0f7a54984
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2022-02-13 10:19:06 +0100

    Update random battleground with 1 second delay.

--- Business/BattlegroundFinder/RandomBattleground.lua
+++ Business/BattlegroundFinder/RandomBattleground.lua
@@ -9,14 +9,16 @@ function GAFE.RandomBattleground.Init()
     extender:Init()
 
     local function OnActivityFinderStatusUpdate(_, previousState, nextState)
-        local characterId = GetCurrentCharacterId()
-        local isRewardAvailableByTimer = RandomActivityExtender_GetTimeUntilNextReward(characterId, extender.rewardsVars) <= 0
-        -- There are several battleground activities but so far they all share the reward.
-        local isRewardAvailableByZos = IsActivityEligibleForDailyReward(LFG_ACTIVITY_BATTLE_GROUND_NON_CHAMPION)
-
-        if nextState == BATTLEGROUND_STATE_POSTGAME and isRewardAvailableByTimer and not isRewardAvailableByZos then
-            extender.rewardsVars.randomRewards[characterId] = GetTimeStamp()
-        end
+        -- Check 1 second later so IsActivityEligibleForDailyReward is ready.
+        zo_callLater(function()
+            local characterId = GetCurrentCharacterId()
+            local isRewardAvailableByTimer = RandomActivityExtender_GetTimeUntilNextReward(characterId, extender.rewardsVars) <= 0
+            -- There are several battleground activities but so far they all share the reward.
+            local isRewardAvailableByZos = IsActivityEligibleForDailyReward(LFG_ACTIVITY_BATTLE_GROUND_NON_CHAMPION)
+            if nextState == BATTLEGROUND_STATE_POSTGAME and isRewardAvailableByTimer and not isRewardAvailableByZos then
+                extender.rewardsVars.randomRewards[characterId] = GetTimeStamp()
+            end
+        end, 1000)
     end
 
     EM:RegisterForEvent(extender.name.."Activity_Update", EVENT_BATTLEGROUND_STATE_CHANGED, OnActivityFinderStatusUpdate)
