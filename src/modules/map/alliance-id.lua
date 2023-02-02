9daf8da564f84cc6e7893880b6b51ca3f7280db9
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2023-02-02 19:42:35 +0100

    Add alliance wayshrines to map.

---
+++ src/modules/map/alliance-id.lua
@@ -0,0 +1,5 @@
+GAFE_ALLIANCE_ID = {
+    Aldmeri = 1,
+    Daggerfall = 2,
+    Ebonheart = 3
+}
--- src/modules/map/map.lua
+++ src/modules/map/map.lua
@@ -15,10 +15,12 @@ function GAFE_MAP.Init()
     end
 
     local function OnShown()
-        local nodeIndex = GAFE.SavedVars.map.favourite
-        local knownNode, name = GetFastTravelNodeInfo(nodeIndex)
+        local favouriteNode = GAFE.SavedVars.map.favourite
+        local _, favouriteNodeName = GetFastTravelNodeInfo(favouriteNode)
 
-        local function TeleportToHome()
+        local function TeleportTo(nodeIndex)
+            local knownNode, name = GetFastTravelNodeInfo(nodeIndex)
+
             if knownNode then
                 if isFastTravel then
                     ZO_Dialogs_ShowPlatformDialog("FAST_TRAVEL_CONFIRM", {nodeIndex = nodeIndex}, {mainTextParams = {name}})
@@ -30,13 +32,45 @@ function GAFE_MAP.Init()
 
         keybindStripGroup = {
             {
-                name = name,
+                name = favouriteNodeName,
                 keybind = "UI_SHORTCUT_QUATERNARY",
-                callback = function() TeleportToHome() end,
+                callback = function() TeleportTo(favouriteNode) end,
             },
             alignment = KEYBIND_STRIP_ALIGN_CENTER,
         }
         KEYBIND_STRIP:AddKeybindButtonGroup(keybindStripGroup)
+
+        -- Add Fast travel to alliances
+        local function FastTravelToAllianceCity(nodeIndex, allianceId, parent)
+            local size = 30
+            local texture, position
+            if allianceId == GAFE_ALLIANCE_ID.Aldmeri then
+                texture = "/esoui/art/ava/ava_allianceflag_aldmeri.dds"
+                position = 0
+            elseif allianceId == GAFE_ALLIANCE_ID.Daggerfall then
+                texture = "/esoui/art/ava/ava_allianceflag_daggerfall.dds"
+                position = size * 2
+            elseif allianceId == GAFE_ALLIANCE_ID.Ebonheart then
+                texture = "/esoui/art/ava/ava_allianceflag_ebonheart.dds"
+                position = size * 4
+            end
+    
+            local knownNode, name = GetFastTravelNodeInfo(nodeIndex)
+            local xOffset = 4
+            GAFE.UI.Texture(parent:GetName().."GAFE_Label"..allianceId, parent, {size,size*2}, {TOPLEFT,parent,TOPLEFT,position + xOffset, -45}, texture)
+            local button = GAFE.UI.Button(parent:GetName().."GAFE_Button"..allianceId, parent, {size*1.2,size*1.2}, {TOPLEFT,parent,TOPLEFT,position + xOffset + size - 10, -47}, nil, function() TeleportTo(nodeIndex) end, true, name)
+            if knownNode then
+                button:SetNormalTexture("/esoui/art/icons/poi/poi_wayshrine_complete.dds")
+                button:SetMouseOverTexture("/esoui/art/icons/poi/poi_wayshrine_glow.dds")
+            else
+                button:SetNormalTexture("/esoui/art/icons/poi/poi_wayshrine_incomplete.dds")
+            end
+        end
+
+        local parent = ZO_WorldMapInfo
+        FastTravelToAllianceCity(214, GAFE_ALLIANCE_ID.Aldmeri, parent)
+        FastTravelToAllianceCity(56, GAFE_ALLIANCE_ID.Daggerfall, parent)
+        FastTravelToAllianceCity(28, GAFE_ALLIANCE_ID.Ebonheart, parent)
     end
 
     local function OnHidden()
@@ -49,36 +83,3 @@ function GAFE_MAP.Init()
     EVENT_MANAGER:RegisterForEvent(GAFE.name .. "_Map", EVENT_START_FAST_TRAVEL_INTERACTION, OnFastTravelStart)
     EVENT_MANAGER:RegisterForEvent(GAFE.name .. "_Map", EVENT_END_FAST_TRAVEL_INTERACTION, OnFastTravelEnd)
 end
-
-
--- TODO: Add to map
--- local function FastTravelToAllianceCity(nodeIndex, allianceId, parent)
--- 	if nodeIndex then
---         local knownNode, name = GetFastTravelNodeInfo(nodeIndex)
---         local size = 30
--- 		local texture, position
--- 		if allianceId == AllianceId.Aldmeri then
--- 			texture = "/esoui/art/ava/ava_allianceflag_aldmeri.dds"
--- 			position = 0
--- 		elseif allianceId == AllianceId.Daggerfall then
--- 			texture = "/esoui/art/ava/ava_allianceflag_daggerfall.dds"
--- 			position = size * 2
--- 		elseif allianceId == AllianceId.Ebonheart then
--- 			texture = "/esoui/art/ava/ava_allianceflag_ebonheart.dds"
--- 			position = size * 4
--- 		end
-
--- 		GAFE.UI.Texture(parent:GetName().."Label"..allianceId, parent, {size,size*2}, {TOPLEFT,parent,TOPLEFT,position, -40}, texture)
---         local button = GAFE.UI.Button(parent:GetName().."Button"..allianceId, parent, {size*1.2,size*1.2}, {TOPLEFT,parent,TOPLEFT,position+size-10, -40}, nil, function() finderActivityExtender:FastTravel(nodeIndex, name) end, knownNode)
---         if knownNode then
---             button:SetNormalTexture("/esoui/art/icons/poi/poi_wayshrine_complete.dds")
---         else
---             button:SetNormalTexture("/esoui/art/icons/poi/poi_wayshrine_incomplete.dds")
---         end
---     end
--- end
-
-
--- FastTravelToAllianceCity(214, AllianceId.Aldmeri, parent)
--- FastTravelToAllianceCity(56, AllianceId.Daggerfall, parent)
--- FastTravelToAllianceCity(28, AllianceId.Ebonheart, parent)
