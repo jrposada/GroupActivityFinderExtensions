ef0c8ba15bc533dac0b66c593d71cfb8a6095cc7
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2023-07-02 23:08:27 +0200

    Add version script.

---
+++ version.ps1
@@ -0,0 +1,45 @@
+param (
+    [Parameter(Mandatory = $true)][ValidateSet("major", "minor", "patch")][String]$mode
+)
+
+$filePath = "./GroupActivityFinderExtensions.txt";
+
+[System.Collections.ArrayList]$file = [System.Collections.ArrayList]::New();
+
+foreach ($line in [System.IO.File]::ReadLines($filePath)) {
+    if ($line.StartsWith("## AddOnVersion:")) {
+        $version = $line.Split(" ")[2]
+        $major = [int]$version.Substring(0, 2)
+        $minor = [int]$version.Substring(2, 2)
+        $patch = [int]$version.Substring(4, 2)
+
+        switch ($mode) {
+            "major" { $major += 1 }
+            "minor" { $minor += 1 }
+            "patch" { $patch += 1 }
+            Default {}
+        }
+
+        $line = "## AddOnVersion: " + $major.ToString().PadLeft(2, "0") + $minor.ToString().PadLeft(2, "0") + $patch.ToString().PadLeft(2, "0")
+    }
+    
+    if ($line.StartsWith("## Version:")) {
+        $version = $line.Split(" ")[2].Split(".")
+        $major = [int]$version[0]
+        $minor = [int]$version[1]
+        $patch = [int]$version[2]
+
+        switch ($mode) {
+            "major" { $major += 1 }
+            "minor" { $minor += 1 }
+            "patch" { $patch += 1 }
+            Default {}
+        }
+
+        $line = "## Version: " + [String]::Join(".", @($major, $minor, $patch))
+    }
+
+    $file.Add($line) > $null
+}
+
+Set-Content -Path $filePath -Encoding UTF8 -Value $file
