7fc770eb76f371846e85c85f6643dd2907c95bf9
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2023-07-02 13:25:51 +0200

    Ignore crafting writs.

--- src/modules/quest-automation/quest-automation.lua
+++ src/modules/quest-automation/quest-automation.lua
@@ -4,6 +4,7 @@ local LQD_Internal = _G["LibQuestData_Internal"]
 
 GAFE_DIALY_NPC_NAME = {}
 GAFE_PLEDGE_NPC_NAME = {}
+GAFE_CRAFTING_WRITS_NPC_NAME = {}
 
 GAFE_QUEST_AUTOMATION = {}
 
@@ -18,9 +19,19 @@ local function contains(data, value)
 end
 
 --- Helper to get if pledge id is from a pledge. Only checks for one quest per character name.
---- Used to get the quest given name only.
+--- Used to get the quest giver name.
+---@param questId any
+---@return boolean
 local function IsPledge(questId)
     return questId == GAFE_PLEDGE_ID.EldenHollowII or questId == GAFE_PLEDGE_ID.Volenfell or questId == GAFE_PLEDGE_ID.ImperialCityPrison
+end
+
+--- Check if quest id is one of the crafting writs. Only checks for one of each type.
+--- Used to get the quest giver name.
+---@param questId any
+---@return boolean
+local function IsCraftingWrit(questId)
+    return questId == 5394 or questId == 5415
 end
 
 function GAFE_QUEST_AUTOMATION.Init()
@@ -32,17 +43,18 @@ function GAFE_QUEST_AUTOMATION.Init()
             if questRepeat == LQD.quest_data_repeat.quest_repeat_daily or questRepeat == LQD.quest_data_repeat.quest_repeat_repeatable then
                 local npcName = LQD:get_quest_giver(questPinData[LQD.quest_map_pin_index.quest_giver], GAFE.lang)
                 local isPledge = IsPledge(questId)
-                -- todo check for duplicates.
-                if not isPledge and not contains(GAFE_DIALY_NPC_NAME, npcName) then
-                    table.insert(GAFE_DIALY_NPC_NAME, npcName)
-                elseif isPledge and not contains(GAFE_PLEDGE_NPC_NAME, npcName) then
+                local isCraftingWrit = IsCraftingWrit(questId)
+
+                if isPledge and not contains(GAFE_PLEDGE_NPC_NAME, npcName) then
                     table.insert(GAFE_PLEDGE_NPC_NAME, npcName)
+                elseif isCraftingWrit and not contains(GAFE_CRAFTING_WRITS_NPC_NAME, npcName) then
+                    table.insert(GAFE_CRAFTING_WRITS_NPC_NAME, npcName)
+                elseif not contains(GAFE_DIALY_NPC_NAME, npcName) then
+                    table.insert(GAFE_DIALY_NPC_NAME, npcName)
                 end
             end
         end
     end
-
-    GAFE.LogLater(GAFE_PLEDGE_NPC_NAME)
 
     GAFE_QUEST_AUTOMATION.AutomaticallyHandleQuests(
         GAFE.SavedVars.dungeons.handlePledgeQuest
@@ -92,8 +104,7 @@ function GAFE_QUEST_AUTOMATION.AutomaticallyHandleQuests(enable)
             EndInteraction(INTERACTION_CONVERSATION)
         end
 
-        -- FIXME infinite loop with new GAFE_DIALY_NPC_NAME var
-        if contains(GAFE_DIALY_NPC_NAME, npcName) then
+        if contains(GAFE_DIALY_NPC_NAME, npcName) and not contains(GAFE_CRAFTING_WRITS_NPC_NAME, npcName) then
             if optionCount ~= 0 then
                 for optionIndex = 1, optionCount + 1 do
                     local optionString, optionType = GetChatterOption(optionIndex)
