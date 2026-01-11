e646ade5c4e8037f3bb0ffcfaa8b5198342911cc
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2026-01-11 13:09:58 +0100

    fix: lib panicida integration

--- src/modules/activity-finder/schedule/schedule.lua
+++ src/modules/activity-finder/schedule/schedule.lua
@@ -3,18 +3,22 @@ local WM = WINDOW_MANAGER
 
 GAFE_SCHEDULE = {}
 
-function GAFE_SCHEDULE.Init()
-    local control = _G["GAFE_ActivitySchedulePanel"] or
-        WM:CreateControlFromVirtual('GAFE_ActivitySchedulePanel', GuiRoot, "GAFE_ActivitySchedulePanel")
-    local fragment = ZO_FadeSceneFragment:New(control)
+local ZO_ACTIVITY_FINDER_SORT_PRIORITY = ZO_ACTIVITY_FINDER_SORT_PRIORITY
 
-    local priority = LibPanicida.Constants.ACTIVITY_FINDER_SORT_PRIORITY.ACTIVITY_SCHEDULE
-    GROUP_MENU_KEYBOARD:AddCategory({
-        priority = priority,
-        name = GAFE.Loc("ActivitySchedule"),
-        categoryFragment = fragment,
-        normalIcon = "esoui/art/journal/journal_tabicon_cadwell_up.dds",
-        pressedIcon = "esoui/art/journal/journal_tabicon_cadwell_down.dds",
-        mouseoverIcon = "esoui/art/journal/journal_tabicon_cadwell_over.dds",
-    })
+local BASE_DUNGEONS_PRIORITY = ZO_ACTIVITY_FINDER_SORT_PRIORITY.DUNGEONS
+local ACTIVITY_PRIORITY = BASE_DUNGEONS_PRIORITY + 1000
+
+function GAFE_SCHEDULE.Init()
+  local control = _G["GAFE_ActivitySchedulePanel"] or
+      WM:CreateControlFromVirtual('GAFE_ActivitySchedulePanel', GuiRoot,
+        "GAFE_ActivitySchedulePanel")
+  local fragment = ZO_FadeSceneFragment:New(control)
+  GROUP_MENU_KEYBOARD:AddCategory({
+    priority = ACTIVITY_PRIORITY,
+    name = GAFE.Loc("ActivitySchedule"),
+    categoryFragment = fragment,
+    normalIcon = "esoui/art/journal/journal_tabicon_cadwell_up.dds",
+    pressedIcon = "esoui/art/journal/journal_tabicon_cadwell_down.dds",
+    mouseoverIcon = "esoui/art/journal/journal_tabicon_cadwell_over.dds",
+  })
 end
