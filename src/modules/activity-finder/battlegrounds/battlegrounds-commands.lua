23449d8e5e2b94b15a83b99b1d50096a912f4f16
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2026-01-14 19:31:45 +0100

    feat: rename queue battleground command

--- src/modules/activity-finder/battlegrounds/battlegrounds-commands.lua
+++ src/modules/activity-finder/battlegrounds/battlegrounds-commands.lua
@@ -18,8 +18,12 @@ local BattlegroundCommands = {}
 -- Private Functions
 -- =============================================================================
 
+-- =============================================================================
+-- Public Functions
+-- =============================================================================
+
 --- Queues a random battleground based on player eligibility.
-local function battleground()
+function BattlegroundCommands.Battleground()
   local activityType = nil
   if GetUnitLevel("player") ~= 50 then
     activityType = LFG_ACTIVITY_BATTLE_GROUND_LOW_LEVEL
@@ -29,16 +33,11 @@ local function battleground()
   QueueManager.QueueByActivityType(activityType)
 end
 
-local commandsList = {
-  { name = "/bg", func = battleground },
-}
-
--- =============================================================================
--- Public Functions
--- =============================================================================
-
 --- Initializes slash commands for battleground queuing.
 function BattlegroundCommands.Init()
+  local commandsList = {
+    { name = "/qb", func = BattlegroundCommands.Battleground },
+  }
   for _, param in pairs(commandsList) do
     SLASH_COMMANDS[param.name] = param.func
   end
