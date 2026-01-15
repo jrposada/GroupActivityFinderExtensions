55b4e272e7077046ec60cf1362974383895087fc
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2026-01-16 00:07:52 +0100

    fix: move schedule outside of globals

--- src/modules/activity-finder/schedule/schedule.lua
+++ src/modules/activity-finder/schedule/schedule.lua
@@ -1,18 +1,32 @@
+-- Localized Globals
 local GAFE = GroupActivityFinderExtensions
-local WM = WINDOW_MANAGER
-
-GAFE_SCHEDULE = {}
-
+local WINDOW_MANAGER = WINDOW_MANAGER
+local GROUP_MENU_KEYBOARD = GROUP_MENU_KEYBOARD
+local ZO_FadeSceneFragment = ZO_FadeSceneFragment
 local ZO_ACTIVITY_FINDER_SORT_PRIORITY = ZO_ACTIVITY_FINDER_SORT_PRIORITY
+local GuiRoot = GuiRoot
 
+-- Constants
 local BASE_DUNGEONS_PRIORITY = ZO_ACTIVITY_FINDER_SORT_PRIORITY.DUNGEONS
 local ACTIVITY_PRIORITY = BASE_DUNGEONS_PRIORITY + 1000
 
-function GAFE_SCHEDULE.Init()
+-- Module Declaration
+local Schedule = {}
+
+-- Public Functions
+
+--- Initializes the Activity Schedule panel and registers it with the group menu.
+--- Creates the UI control and adds the schedule category to GROUP_MENU_KEYBOARD.
+function Schedule.Init()
   local control = _G["GAFE_ActivitySchedulePanel"] or
-      WM:CreateControlFromVirtual('GAFE_ActivitySchedulePanel', GuiRoot,
-        "GAFE_ActivitySchedulePanel")
+      WINDOW_MANAGER:CreateControlFromVirtual(
+        'GAFE_ActivitySchedulePanel',
+        GuiRoot,
+        "GAFE_ActivitySchedulePanel"
+      )
+
   local fragment = ZO_FadeSceneFragment:New(control)
+
   GROUP_MENU_KEYBOARD:AddCategory({
     priority = ACTIVITY_PRIORITY,
     name = GAFE.Loc("ActivitySchedule"),
@@ -22,3 +36,6 @@ function GAFE_SCHEDULE.Init()
     mouseoverIcon = "esoui/art/journal/journal_tabicon_cadwell_over.dds",
   })
 end
+
+-- Module Registration
+GAFE.Schedule = Schedule
